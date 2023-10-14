194644466ca643adcd53fed41a5a0e34c26222f3
Shayan Rahimi <1shayanrsh@gmail.com>
2023-10-14 19:16:06 +0330

    final version

--- Blackjack Game/Blackjack.py
+++ Blackjack Game/Blackjack.py
@@ -85,28 +85,30 @@ def win_or_lose(user_score, computer_score):
 
 
 def blackjack_game():
+  
   user_cards = []
   computer_cards = []
   user_score = 0
   computer_score = 0
   want_play = True
+  
   if input("Do you want to play a game of Blackjack? Type 'y' or 'n': ").lower() == 'n':
     want_play = False
+    return
+  else:
+    os.system('cls')
+  
+  print(logo)
+  cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    
+  user_cards = random.sample(cards, 2)
+  user_score = score_and_isAce(user_cards)
+  print(f"Your cards: {user_cards}, current score: {user_score}")
+  computer_cards = random.sample(cards, 2)
+  computer_score = score_and_isAce(computer_cards)
+  print(f"Computer's first card: {[computer_cards[0]]}")
 
   while want_play:
-    os.system('cls')
-    print(logo)
-  
-    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-    
-    user_cards = random.sample(cards, 2)
-    user_score = score_and_isAce(user_cards)
-    print(f"Your cards: {user_cards}, current score: {user_score}")
-    computer_cards = random.sample(cards, 2)
-    computer_score = score_and_isAce(computer_cards)
-    print(f"Computer's first card: {[computer_cards[0]]}")
-
-
     want_new_card = input("Type 'y' to get another card, type 'n' to pass: ").lower()
     
     if want_new_card == 'n':
@@ -120,7 +122,22 @@ def blackjack_game():
       
       win_or_lose(user_score, computer_score)
       blackjack_game()
-    # elif want_new_card == 'y':
+    elif want_new_card == 'y':
+      user_cards.append(random.choice(cards))
+      user_score = score_and_isAce(user_cards)
+      print(f"Your cards: {user_cards}, current score: {user_score}")
+      print(f"Computer's first card: {[computer_cards[0]]}")
+
+      if user_score > 21:
+        while computer_score < 21 and computer_score <= user_score:
+          computer_cards.append(random.choice(cards))
+          computer_score = score_and_isAce(computer_cards)
+
+        print(f"Your final hand: {user_cards}, final score: {user_score}")
+        print(f"Computer's final hand: {computer_cards}, final score: {computer_score}")
+
+        win_or_lose(user_score, computer_score)
+        blackjack_game()
       
       
 
