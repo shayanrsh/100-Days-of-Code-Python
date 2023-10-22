37e64c1ce2c7bea9771351e3f919a86f8e624176
Shayan Rahimi <1shayanrsh@gmail.com>
2023-10-22 18:44:16 +0330

    final version

--- Day 25 - US State Game/main.py
+++ Day 25 - US State Game/main.py
@@ -14,10 +14,20 @@ my_turtle.penup()
 my_turtle.hideturtle()
 
 data = pd.read_csv("50_states.csv")
+guessed_states = []
 
 while score < 50:
     answer_state = screen.textinput(title=f"score {score}/50", prompt="What's another state's name?").title()
+
+    if answer_state == "Exit":
+        all_states = data.state.to_list()
+        not_guessed_states = [x for x in all_states if x not in guessed_states]
+        missing_states = pd.DataFrame(not_guessed_states)
+
+        missing_states.to_csv("missing_states.csv")
+        break
     if answer_state in data['state'].values:
+        guessed_states.append(answer_state)
         score += 1
         # Filter the DataFrame for the state 'California'
         california_data = data[data['state'] == answer_state]
@@ -29,6 +39,3 @@ while score < 50:
         my_turtle.goto(x, y)
         my_turtle.write(answer_state)
         screen.title(f"U.S. States Game. Current score {score}/50")
-
-
-screen.exitonclick()
