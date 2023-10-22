877db65a287862a4f1be5fe6c3fe182b2e96b0a5
Shayan Rahimi <1shayanrsh@gmail.com>
2023-10-22 18:24:37 +0330

    final version

---
+++ Day 25 - US State Game/main.py
@@ -0,0 +1,34 @@
+import turtle
+
+import pandas as pd
+
+screen = turtle.Screen()
+score = 0
+screen.title(f"U.S. States Game.")
+
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+my_turtle = turtle.Turtle()
+my_turtle.penup()
+my_turtle.hideturtle()
+
+data = pd.read_csv("50_states.csv")
+
+while score < 50:
+    answer_state = screen.textinput(title=f"score {score}/50", prompt="What's another state's name?").title()
+    if answer_state in data['state'].values:
+        score += 1
+        # Filter the DataFrame for the state 'California'
+        california_data = data[data['state'] == answer_state]
+
+        # Get the 'x' and 'y' values for California
+        x = california_data['x'].values[0]
+        y = california_data['y'].values[0]
+
+        my_turtle.goto(x, y)
+        my_turtle.write(answer_state)
+        screen.title(f"U.S. States Game. Current score {score}/50")
+
+
+screen.exitonclick()
