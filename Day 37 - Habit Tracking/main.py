53a5148da3ee0fe911d9b380dd42d6dfd813a3c9
Shayan Rahimi <1shayanrsh@gmail.com>
2023-11-05 10:46:48 +0330

    final version

---
+++ Day 37 - Habit Tracking/main.py
@@ -0,0 +1,63 @@
+import requests
+from datetime import datetime
+
+TOKEN = "lkh3423kj23vb4i"
+USERNAME = "shayanr"
+
+
+pixela_endpoint = "https://pixe.la/v1/users"
+
+user_params = {
+    "token": TOKEN,
+    "username": USERNAME,
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes",
+}
+
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_config = {
+    "id": "graph1",
+    "name": "Cycling graph",
+    "unit": "km",
+    "type": "float",
+    "color": "ajisai"
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# print(response.text)
+
+post_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/graph1"
+
+today = datetime(year=2023, month=11, day=4)
+
+post_config = {
+    "date": today.strftime("%Y%m%d"),
+    "quantity": "9",
+}
+
+# response = requests.post(url=post_endpoint, json=post_config, headers=headers)
+# print(response.text)
+
+update_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/graph1/{today.strftime('%Y%m%d')}"
+
+update_config = {
+    "quantity": "1"
+}
+
+# response = requests.put(url=update_endpoint, json=update_config, headers=headers)
+# print(response.text)
+
+delete_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/graph1/{today.strftime('%Y%m%d')}"
+
+response = requests.delete(url=delete_endpoint, headers=headers)
+print(response.text)
+
+
