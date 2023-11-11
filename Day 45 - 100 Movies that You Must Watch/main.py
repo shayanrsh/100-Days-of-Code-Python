83abbde74dc13fbc5a7de65c13dc4df9e3c52af4
Shayan Rahimi <1shayanrsh@gmail.com>
2023-11-11 08:36:01 +0330

    final version

---
+++ Day 45 - 100 Movies that You Must Watch/main.py
@@ -0,0 +1,20 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
+
+# Write your code below this line 👇
+response = requests.get(URL)
+response.raise_for_status()
+data = response.text
+
+soup = BeautifulSoup(data, "html.parser")
+movies = soup.find_all(name="h3", class_="title")
+movie_titles = [movie.getText() for movie in movies]
+movie_titles.reverse()
+
+with open("movies.txt", mode="w", encoding="utf-8") as file:
+    for movie in movie_titles:
+        file.write(f"{movie}\n")
+
+
