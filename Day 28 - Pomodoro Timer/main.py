beb2ae5f8286b38a1ea069ec3c01e1854c63bf96
Shayan Rahimi <sshahmirzadi21@ku.edu.tr>
2024-02-11 10:27:30 +0330

    final version

--- Day 28 - Pomodoro Timer/main.py
+++ Day 28 - Pomodoro Timer/main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -6,114 +7,86 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
+WORK_MIN = 1
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
+timer = None
 
-# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER RESET ------------------------------- #
+
+def reset_timer():
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    title_label.config(text="Timer")
+    check_marks.config(text="")
+    global reps
+    reps = 0
+
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def start_timer():
+    global reps
+    reps += 1
+
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    if reps % 8 == 0:
+        count_down(long_break_sec)
+        title_label.config(text="Break", fg=RED)
+    elif reps % 2 == 0:
+        count_down(short_break_sec)
+        title_label.config(text="Break", fg=PINK)
+    else:
+        count_down(work_sec)
+        title_label.config(text="Work", fg=GREEN)
+
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+def count_down(count):
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        global timer
+        timer = window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+        marks = ""
+        work_sessions = math.floor(reps / 2)
+        for _ in range(work_sessions):
+            marks += "✔"
+        check_marks.config(text=marks)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
-
-# I know this program is not optimized and might be buggy.
-# But I don't want to copy and paste the solution to preserve my original efforts.
-
 window = Tk()
 window.title("Pomodoro")
-window.config(pady=50, padx=100, bg=YELLOW)
+window.config(padx=100, pady=50, bg=YELLOW)
 
-timer_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 35, "bold"))
-timer_label.grid(row=0, column=1)
-
-check_mark_label = Label(text="✅", fg=GREEN, bg=YELLOW)
-check_mark_label.config(pady=30, bg=YELLOW)
-check_mark_label.grid(row=2, column=1)
+title_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50))
+title_label.grid(column=1, row=0)
 
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
-
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.grid(row=1, column=1)
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(column=1, row=1)
 
-tomato_img = PhotoImage(file="tomato.png")
-canvas.create_image(100, 112, image=tomato_img)
-text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.grid(row=1, column=1)
+start_button = Button(text="Start", highlightthickness=0, command=start_timer)
+start_button.grid(column=0, row=2)
 
-round_count = 1
+reset_button = Button(text="Reset", highlightthickness=0, command=reset_timer)
+reset_button.grid(column=2, row=2)
 
-
-def reset_window():
-    canvas.itemconfig(text, text="00:00")
-    check_mark_label.config(text="✅", fg=GREEN, bg=YELLOW)
-    global round_count
-    round_count = 1
-
-
-def timer_func():
-    for minute in range(24, -1, -1):
-        for second in range(59, -1, -1):
-            if second < 10 and minute < 10:
-                canvas.itemconfig(text, text=f"0{minute}:0{second}")
-            elif second < 10:
-                canvas.itemconfig(text, text=f"{minute}:0{second}")
-            elif minute < 10:
-                canvas.itemconfig(text, text=f"0{minute}:{second}")
-            else:
-                canvas.itemconfig(text, text=f"{minute}:{second}")
-            window.update()
-            window.after(1000)
-
-
-def short_break_time():
-    for minute in range(4, -1, -1):
-        for second in range(59, -1, -1):
-            if second < 10 and minute < 10:
-                canvas.itemconfig(text, text=f"0{minute}:0{second}")
-            elif second < 10:
-                canvas.itemconfig(text, text=f"{minute}:0{second}")
-            elif minute < 10:
-                canvas.itemconfig(text, text=f"0{minute}:{second}")
-            else:
-                canvas.itemconfig(text, text=f"{minute}:{second}")
-            window.update()
-            window.after(1000)
-
-
-def long_break_time():
-    for minute in range(14, -1, -1):
-        for second in range(59, -1, -1):
-            if second < 10 and minute < 10:
-                canvas.itemconfig(text, text=f"0{minute}:0{second}")
-            elif second < 10:
-                canvas.itemconfig(text, text=f"{minute}:0{second}")
-            elif minute < 10:
-                canvas.itemconfig(text, text=f"0{minute}:{second}")
-            else:
-                canvas.itemconfig(text, text=f"{minute}:{second}")
-            window.update()
-            window.after(1000)
-
-
-def start():
-    global round_count
-    for _ in range(4):
-        timer_func()
-        check_mark_label.config(text="✅" * round_count, fg=GREEN, bg=YELLOW)
-        round_count += 1
-        short_break_time()
-    long_break_time()
-    reset_window()
-
-
-start_button = Button(text="Start", command=start)
-start_button.grid(row=2, column=0)
-
-reset_button = Button(text="Reset", command=reset_window)
-reset_button.grid(row=2, column=2)
+check_marks = Label(fg=GREEN, bg=YELLOW)
+check_marks.grid(column=1, row=3)
 
 window.mainloop()
